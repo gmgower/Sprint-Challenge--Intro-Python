f5e98c214752566a76516978155a5e6201ed675f
@gmgower <gmgower@gmail.com>
2020-05-17 20:50:40 -0500

    Implement fuctionality to print list of cities.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -29,7 +29,11 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+
+  with open('cities.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+      cities.append(City(row[0], row[3], row[4]))    
     return cities
 
 cityreader(cities)
